625cf8e7df330b345d2e3e86456b91cb776a0ab3
christina kaliora <develop@pirron.one>
2019-12-03 21:00:27 +0100

    finished 2 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+// MOUSEOVER - MOUSEOUT
+const menuElement = document.querySelector('h1')
+
+menuElement.addEventListener('mouseover', (e)=> {
+    e.target.style.color = '#d6c31b';
+});
+
+menuElement.addEventListener('mouseout', (e)=> {
+    e.target.style.color = 'black';
+});
+
+
+// WHEEL
+let img = document.querySelector('header > img');
+
+let rotation = 0;
+img.addEventListener('wheel', (e) => {
+     img.style.transform = 'rotate(180deg)';
+});
